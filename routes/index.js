dec17849d99a6afef9697d41e831bb855a9af99d
bibi1989 <bibiaremieye@gmail.com>
2020-04-06 14:56:15 +0100

    created fetching all endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+const Cars = require("../models/carsModel");
+
+/* GET home page. */
+router.get("/", async (req, res, next) => {
+  try {
+    const cars = await Cars.find();
+    res.json({ cars });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;

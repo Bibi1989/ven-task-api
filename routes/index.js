5833e5b152bb20f461a0b149d093da8cbc656304
bibi1989 <bibiaremieye@gmail.com>
2020-04-06 14:58:00 +0100

    created posting to endpoint

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,22 @@ const router = express.Router();
 const Cars = require("../models/carsModel");
 
 /* GET home page. */
-router.get("/", async (req, res, next) => {
+router.get("/", async (_req, res, next) => {
   try {
     const cars = await Cars.find();
     res.json({ cars });
   } catch (error) {
     next(error);
   }
+});
+
+router.post("/", async function (req, res, next) {
+  console.log(req.body);
+  let cars = new Cars({
+    ...req.body,
+  });
+  await cars.save();
+  res.json({ cars: cars.first_name });
 });
 
 module.exports = router;

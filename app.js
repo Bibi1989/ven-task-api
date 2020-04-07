52b9b658db9d3b1e082d74e0c35c5eb3d6f8c635
bibi1989 <bibiaremieye@gmail.com>
2020-04-07 22:53:45 +0100

    fixes bugs

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ const v = require("./parsing");
 console.log(v);
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -31,7 +30,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/api/cars", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

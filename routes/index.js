3b9b2ad87ca7c31219ed2fef22045d729a9c2a31
bibi1989 <bibiaremieye@gmail.com>
2020-04-07 13:40:29 +0100

    created filtered endpoint

--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,37 @@ const router = express.Router();
 const Cars = require("../models/carsModel");
 
 /* GET home page. */
-router.get("/", async (_req, res, next) => {
+router.get("/", async (req, res, next) => {
+  const noOfLimit = 10;
+  const page = parseInt(req.query.page) - 1;
+  const limit = noOfLimit * page;
+
+  if (page < 0)
+    return res.status(404).json({ error: "Page should not be less than 1" });
+
   try {
-    const cars = await Cars.find();
-    res.json({ cars });
+    const cars = await Cars.find().skip(limit).limit(noOfLimit);
+    res.json({ pages: page, limits: noOfLimit, cars });
   } catch (error) {
     next(error);
   }
 });
 
-router.post("/", async function (req, res, next) {
-  console.log(req.body);
-  let cars = new Cars({
-    ...req.body,
-  });
-  await cars.save();
-  res.json({ cars: cars.first_name });
+router.post("/query", async (req, res, next) => {
+  console.log(req.body.countries);
+  const queryData = {
+    car_model_year: { $gte: req.body.start_year, $lte: req.body.end_year },
+    gender: req.body.gender,
+    $or: req.body.countries,
+    $or: req.body.colors,
+  };
+
+  try {
+    let car = await Cars.find(queryData);
+    res.json({ car });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
